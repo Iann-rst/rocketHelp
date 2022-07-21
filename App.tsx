df33ea21f3f5b4b6f684f4b12a19ba57ec1f1b65
Iann-rst <iannrst11@gmail.com>
2022-07-21 18:12:03 -0300

    App

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/styles/theme';
+
 import { SignIn } from './src/screens/SignIn';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
+
   return (
-    <NativeBaseProvider>
-      <SignIn />
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   )
 }
