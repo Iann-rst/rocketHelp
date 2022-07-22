e9a313a3b23d1266b14eda374cbbcd99d5d4f9b1
Iann-rst <iannrst11@gmail.com>
2022-07-22 12:11:44 -0300

    feat: Componente Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { Heading, HStack, IconButton, useTheme, StyledProps } from 'native-base';
+import { CaretLeft } from 'phosphor-react-native';
+
+type Props = StyledProps & {
+  title: string;
+}
+
+export function Header({ title }: Props) {
+  const { colors } = useTheme();
+  return (
+    <HStack w="full" justifyContent="space-between" alignItems="center" bg="gray.600" pb={6} pt={12}>
+      <IconButton icon={<CaretLeft color={colors.gray[200]} size={24} />} />
+      <Heading color="gray.100" textAlign="center" fontSize="lg" flex={1} ml={-6}>
+        {title}
+      </Heading>
+    </HStack>
+  );
+}
