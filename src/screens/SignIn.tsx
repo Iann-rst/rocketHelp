3a1ce5e7d0128d7c17ec7de74d49cb106ee0955a
Iann-rst <iannrst11@gmail.com>
2022-07-26 10:36:06 -0300

    feat: criando autenticação do usuário

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import auth from '@react-native-firebase/auth'
+
 import {
   VStack,
   Heading,
@@ -6,22 +8,48 @@ import {
   useTheme
 
 } from 'native-base';
+
 import { Envelope, Key } from 'phosphor-react-native';
-
 import Logo from '../assets/logo_primary.svg';
-import { Button } from '../components/Button';
 
+import { Button } from '../components/Button';
 import { Input } from '../components/Input';
+import { Alert } from 'react-native';
 
 
 export function SignIn() {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const { colors } = useTheme();
 
+  //Autenticação com Firebase/Auth
   function handleSignIn() {
-    console.log("email:", email, "\nsenha:", senha);
+    if(!email || !senha){
+      return Alert.alert('Entrar', 'Informe e-mail e senha.');
+    }
+
+    setIsLoading(true);
+    auth().signInWithEmailAndPassword(email, senha).catch((error)=>{
+      console.log(error.code);
+      setIsLoading(false);
+
+      if(error.code === 'auth/invalid-email'){
+        return Alert.alert('Entrar', 'E-mail inválido!');
+      }
+
+      if(error.code === 'auth/user-not-found'){
+        return Alert.alert('Entrar', 'E-mail ou senha inválida!');
+      }
+
+      if(error.code === 'auth/wrong-password'){
+        return Alert.alert('Entrar', 'E-mail ou senha inválida!');
+      }
+
+      return Alert.alert('Entrar', 'Não foi possível acessar');
+    })
   }
 
   return (
@@ -49,7 +77,12 @@ export function SignIn() {
         onChangeText={setSenha}
       />
 
-      <Button title="Entrar" w="full" onPress={handleSignIn} />
+      <Button 
+      title="Entrar" 
+      w="full" 
+      onPress={handleSignIn}
+      isLoading={isLoading} 
+      />
     </VStack>
   );
 }
